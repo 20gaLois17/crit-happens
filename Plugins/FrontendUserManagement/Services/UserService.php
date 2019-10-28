df66132bba0a777b700bf5063e8c66dda20ebd17
Basti <sk@7pkonzepte.de>
2019-10-28 09:48:33 +0100

    added service method to create users from console

--- Plugins/FrontendUserManagement/Services/UserService.php
+++ Plugins/FrontendUserManagement/Services/UserService.php
@@ -5,10 +5,51 @@ namespace FrontendUserManagement\Services;
 use Doctrine\ORM\ORMException;
 use FrontendUserManagement\Models\User;
 use Oforge\Engine\Modules\Core\Abstracts\AbstractDatabaseAccess;
+use Oforge\Engine\Modules\Core\Helper\SessionHelper;
 
 class UserService extends AbstractDatabaseAccess {
     public function __construct() {
         parent::__construct(['default' => User::class]);
+    }
+
+    const CLASSES = [
+        'warrior', 'mage', 'priest', 'druid', 'shaman', 'warlock', 'hunter', 'rogue',
+    ];
+
+    /**
+     * @param string $userName
+     * @param string $password
+     * @param string $class
+     *
+     * @throws ORMException
+     */
+    public function createUserEntry(string $userName, string $password, string $class) {
+
+        /** TODO: check if class is valid */
+
+        /** @var User $user */
+        $user = new User();
+        $user->setEmail($userName)->setClass($class);
+        $user->setGuid(SessionHelper::generateGuid());
+        $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+        $user->setPassword($passwordHash);
+        $user->setActive(true);
+        $this->entityManager()->create($user);
+
+        $user = $user->toArray(2, ['createdAt', 'updatedAt', 'guid', 'password']);
+        $user['password'] = $password;
+        print_r($user);
+    }
+
+    /**
+     * @param array $userData
+     *
+     * @throws ORMException
+     */
+    public function createUserEntries(array $userData) {
+        foreach ($userData as $user) {
+            $this->createUserEntry($user[0], $user[1], $user[2]);
+        }
     }
 
     /**
@@ -31,6 +72,7 @@ class UserService extends AbstractDatabaseAccess {
     public function getUsers() : array {
         $users  = $this->repository()->findAll();
         $result = [];
+        /** @var User $user */
         foreach ($users as $user) {
             $result[] = $user->toArray();
         }
